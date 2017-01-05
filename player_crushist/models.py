def163fc18546f62bce0ef2def900400f4eed357
Maxx Lehmann <mglehman@mtu.edu>
2017-01-05 00:44:47 -0600

    added basic models

--- player_crushist/models.py
+++ player_crushist/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Song(models.Model):
+    song_title = models.CharField(max_length=250)
+    song_url = models.URLField(max_length=500)
+    song_votes = models.IntegerField(default=0)
+
+
+class Event(models.Model):
+    event_name = models.CharField(max_length=250)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+
+class User(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user_name = models.CharField(max_length=25)
