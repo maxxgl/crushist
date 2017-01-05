abc6069ea7f4ed06cb0858727c16d37f3066079d
Maxx Lehmann <mglehman@mtu.edu>
2017-01-05 01:15:45 -0600

    new views

--- player_crushist/urls.py
+++ player_crushist/urls.py
@@ -3,5 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.home, name='home'),
+    url(r'^/events/(?P<question_id>[0-9]+)/$', views.events, name='events'),
+    url(r'^/users/(?P<question_id>[0-9]+)/$', views.users, name='users'),
 ]
--- player_crushist/views.py
+++ player_crushist/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
+def home(request):
 
     # return response_with_context(context)
-    return HttpResponse("<h1>Yeah we know it works")
+    return HttpResponse("<h1>Yeah we know it works - home")
+
+
+def events(request):
+
+    return HttpResponse("<h1>Yeah we know it works - events")
+
+
+def users(request):
+
+    return HttpResponse("<h1>Yeah we know it works - users")
+
