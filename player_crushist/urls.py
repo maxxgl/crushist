0a664ab7c0e237b2bda29bdf4fdb395856357310
Maxx Lehmann <mglehman@mtu.edu>
2017-01-05 01:24:12 -0600

    added songs to admin and fixed url patterns

--- player_crushist/urls.py
+++ player_crushist/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^/events/(?P<question_id>[0-9]+)/$', views.events, name='events'),
-    url(r'^/users/(?P<question_id>[0-9]+)/$', views.users, name='users'),
+    url(r'^(?P<question_id>[0-9]+)/$', views.events, name='events'),
+    url(r'^user/(?P<question_id>[0-9]+)/$', views.users, name='users'),
 ]
