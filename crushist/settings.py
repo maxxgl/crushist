67c50d1822b8fea6ff5b98f269eb5b4d4681ab61
Maxx Lehmann <mglehman@mtu.edu>
2017-01-29 16:53:04 +0100

    messing with settings

--- crushist/settings.py
+++ crushist/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'channels',
 ]
 
@@ -78,9 +79,9 @@ WSGI_APPLICATION = 'crushist.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
+        dj_database_url.config(
+            default=config('DATABASE_URL')
+        )
     }
 }
 
@@ -88,15 +89,15 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 
-CHANNEL_LAYERS = {
-    "default": {
-        "BACKEND": "asgi_redis.RedisChannelLayer",
-        "CONFIG": {
-            "hosts": [("localhost", 6379)],
-        },
-        "ROUTING": "crushist.routing.channel_routing",
-    },
-}
+# CHANNEL_LAYERS = {
+#     "default": {
+#         "BACKEND": "asgi_redis.RedisChannelLayer",
+#         "CONFIG": {
+#             "hosts": [("localhost", 6379)],
+#         },
+#         "ROUTING": "crushist.routing.channel_routing",
+#     },
+# }
 
 
 # Password validation
