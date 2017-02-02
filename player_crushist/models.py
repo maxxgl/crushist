52545d021ee7ab47ed2bee9bc4623d2c6ab8ea47
Maxx Lehmann <mglehman@mtu.edu>
2017-02-02 22:41:36 +0100

    updated models

--- player_crushist/models.py
+++ player_crushist/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class User(models.Model):
-    user_name = models.CharField(max_length=25)
+    user_name = models.CharField(max_length=8, primary_key=True)
 
     def __str__(self):
         return self.user_name
@@ -23,9 +23,13 @@ class Song(models.Model):
     title = models.CharField(max_length=250)
     thumbnail_url = models.URLField(max_length=200)
     yt_url = models.URLField(max_length=25)
-    votes = models.IntegerField(default=0)
     added = models.DateTimeField(auto_now_add=True)
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    upvoters = models.ManyToManyField(User, related_name="upvoters")
+    downvoters = models.ManyToManyField(User, related_name="downvoters")
+
+    def votes(self):
+        return self.upvoters.all().count() - self.downvoters.all().count()
 
     def __str__(self):
         return self.title
