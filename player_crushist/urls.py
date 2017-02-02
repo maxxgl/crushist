32a026fe44dadd660e5c7a8b584c51838b33db00
Maxx Lehmann <mglehman@mtu.edu>
2017-02-02 14:45:00 +0100

    moved playlist render to a view partial

--- player_crushist/urls.py
+++ player_crushist/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^(?P<event_id>[0-9]+)/$', views.events, name='events'),
     url(r'^user/(?P<user_id>[0-9]+)/$', views.users, name='users'),
+    url(r'^(?P<event_id>[0-9]+)/playlist/$', views.playlist, name='playlist'),
 ]
