8cd3e67e4e8f8a7cd47519666f51363a3f30810c
Maxx Lehmann <mglehman@mtu.edu>
2017-02-18 20:07:03 +0100

    added google analytics

--- player_crushist/static/player_crushist/cookieSocket.js
+++ player_crushist/static/player_crushist/cookieSocket.js
@@ -8,3 +8,13 @@ function idParse() {
   var b = document.cookie.match('(^|;)\\s*' + "crushistUserId" + '\\s*=\\s*([^;]+)');
   return b ? b.pop() : '';
 }
+
+
+// ***************************** Google Analytics *****************************
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-92216536-1', 'auto');
+ga('send', 'pageview');
