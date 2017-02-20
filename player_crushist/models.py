fc285a210af09d00c26c2948410b9894c9925d12
Maxx Lehmann <mglehman@mtu.edu>
2017-02-20 02:49:32 +0100

    Changed uid back to UUID

--- player_crushist/models.py
+++ player_crushist/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 
 class User(models.Model):
-    id = models.CharField(primary_key=True, unique=True, max_length=40,
-                          default=uuid.uuid4(), editable=False)
+    id = models.UUIDField(
+        primary_key=True, unique=True, default=uuid.uuid4(), editable=False)
     user_name = models.CharField(max_length=20)
 
     def __str__(self):
